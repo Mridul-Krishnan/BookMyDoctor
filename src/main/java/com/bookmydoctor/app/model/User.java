39778b61579dc285ba0b10f368a87286585fb11a
Mridul-Krishnan <mridulkrishnan134@gmail.com>
2021-04-01 23:55:30 +0530

    remove todo tags

--- src/main/java/com/bookmydoctor/app/model/User.java
+++ src/main/java/com/bookmydoctor/app/model/User.java
@@ -1,10 +1,10 @@
-package com.bookmydoctor.app.model;
-
-public class User {
-
-	private int userId;
-	private String userName;
-	private String password;
-	private String role; // admin//doctor //patient
-	
-}
+//package com.bookmydoctor.app.model;
+//
+//public class User {
+//
+//	private int userId;
+//	private String userName;
+//	private String password;
+//	private String role; // admin//doctor //patient
+//	
+//}
--- src/main/java/com/bookmydoctor/app/service/ImplPatientService.java
+++ src/main/java/com/bookmydoctor/app/service/ImplPatientService.java
@@ -24,38 +24,32 @@ public class ImplPatientService implements IPatientService {
 	
 	@Override
 	public Patient addPatient(Patient patient) {
-		// TODO Auto-generated method stub
 		return dao.save(patient);
 	}
 
 	@Override
 	public Patient editPatientProfile(Patient patient) {
-		// TODO Auto-generated method stub
 		return dao.save(patient);
 	}
 
 	
 	@Override
 	public  void removePatientDetails (int patientId) {
-		// TODO Auto-generated method stub
 		dao.deleteById(patientId);
 	}
 
 	@Override
 	public Optional<Patient> getPatient(int patientId) {
-		// TODO Auto-generated method stub
 		return dao.findById(patientId);
 	}
 
 	@Override
 	public List<Patient> getAllPatient() {
-		// TODO Auto-generated method stub
 		return dao.findAll();
 	}
 
 	@Override
 	public List <Doctor> getPatientListByDoctor(int patientId) {
-		// TODO Auto-generated method stub
 		List<Doctor> doctors = (List<Doctor>) dao.findById(patientId).get().getAppointment().getDoctor();
 		doctors.sort((Doctor a, Doctor b)-> a.getDoctorName().compareTo(b.getDoctorName()));
 		return doctors;
@@ -63,7 +57,6 @@ public class ImplPatientService implements IPatientService {
 	
 	@Override
 	public List<Appointment> getAppointmentByPatientId(int patientId) {
-		// TODO Auto-generated method stub
 		List<Appointment> appointments = dao.findAll().stream().map(i -> i.getAppointment()).collect(Collectors.toList());
 		appointments.sort((Appointment i, Appointment j)-> i.getAppointmentDate().compareTo(j.getAppointmentDate()));
 	return appointments;	
